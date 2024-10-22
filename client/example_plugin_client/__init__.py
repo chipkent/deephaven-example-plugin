13f4ca2bd58cc8fab63876b28c16730402c3e97f
Chip Kent <chipkent@deephaven.io>
2024-10-22 12:47:41 -0600

    Cleaned up the client.

--- client/example_plugin_client/__init__.py
+++ client/example_plugin_client/__init__.py
@@ -1,3 +1,6 @@
+
+""" This module provides a client-side interface to the ExampleService server-side object. """
+
 import io
 from typing import Any, List
 from pydeephaven import Table
@@ -6,46 +9,64 @@ import json
 
 
 class ExampleServiceProxy(server_object.ServerObject):
-    def __init__(self, example_plugin: plugin_client.PluginClient):
-        self.type_ = example_plugin.type_
-        self.ticket = example_plugin.ticket
-        self.example_plugin = example_plugin
-        # Consume the first (empty) payload from the server
-        next(self.example_plugin.resp_stream)
+    """
+    This class provides a client-side interface to the ExampleService server-side object.
+
+    When you call a method on this class, it sends a message to the server-side object and returns the result.
+    The inputs are serialized to JSON bytes and sent to the server, and the result is deserialized from JSON and returned.
+    """
+
+    def __init__(self, plugin_client: plugin_client.PluginClient):
+        #TODO: change the assignment to the constructor call in 0.37
+        # super().__init__(type=plugin_client.type_, ticket=plugin_client.ticket)
+        self.type_ = plugin_client.type_
+        self.ticket = plugin_client.ticket
+
+        self.plugin_client = plugin_client
+
+        # Consume the first (empty) payload from the server to acknowledge successful connection
+        next(self.plugin_client.resp_stream)
 
     def echo_string(self, data: str) -> str:
-        message = {'method': 'echo_string', 'data': data}
-        print(f"Sending message: {message}")
-        json_string = json.dumps(message).encode("utf-8")
-        print(f"Sending JSON: {json_string} {type(json_string)}")
+        """ Echo a string. """
 
-        decoded = json_string.decode('utf-8')
-        print(f"DEBUG: {decoded} {type(decoded)}")
-        decoded2 = json.loads(decoded)
-        print(f"DEBUG2: {decoded2} {type(decoded2)}")
+        inputs = {'method': 'echo_string', 'data': data}
 
+        # serialize the inputs to JSON bytes
+        input_bytes = json.dumps(inputs).encode("utf-8")
 
-        self.example_plugin.req_stream.write(json_string, [])
-        result_bytes, result_references = next(self.example_plugin.resp_stream)
+        # no input references
+        input_references = []
 
-        result_json = json.loads(result_bytes.decode("utf-8"))
+        self.plugin_client.req_stream.write(input_bytes, input_references)
+        result_bytes, result_references = next(self.plugin_client.resp_stream)
+        results = json.loads(result_bytes.decode("utf-8"))
 
-        if 'error' in result_json:
-            raise Exception(result_json['error'])
+        if 'error' in results:
+            raise Exception(results['error'])
 
-        return result_json['result']
+        # return the result string
+        return results['result']
     
-    def echo_table(self, table: Table, label: str) -> Table:
-        message = {'method': 'echo_table', 'data': label}
-        json_string = json.dumps(message).encode("utf-8")
-        self.example_plugin.req_stream.write(json_string, [table])
-        result_bytes, result_references = next(self.example_plugin.resp_stream)
-
-        result_json = json.loads(result_bytes.decode("utf-8"))
-
-        if 'error' in result_json:
-            raise Exception(result_json['error'])
+    def echo_table(self, table: Table, data: str) -> Table:
+        """ Echo a table. """
         
+        inputs = {'method': 'echo_table', 'data': data}
+
+        # serialize the inputs to JSON bytes
+        input_bytes = json.dumps(inputs).encode("utf-8")
+
+        # input references
+        input_references = [table]
+
+        self.plugin_client.req_stream.write(input_bytes, input_references)
+        result_bytes, result_references = next(self.plugin_client.resp_stream)
+        results = json.loads(result_bytes.decode("utf-8"))
+
+        if 'error' in results:
+            raise Exception(results['error'])
+        
+        # fetch and return the result table
         return result_references[0].fetch()
 
 
